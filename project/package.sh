dbe531ee882d0e6928998a950406ec4d3547014e
Yaron Sheffer <yaronf.ietf@gmail.com>
2013-02-09 21:55:25 +0200

    Package the Apache-specific files, too

--- project/package.sh
+++ project/package.sh
@@ -3,4 +3,4 @@
 # These should really be installed separately
 libs="djason django_openid_auth"
 
-tar cvzf rfcpop-package.tar.gz --exclude-vcs --exclude "html-rfcs" package.sh README.txt urls.py settings.py rfcpop/ $libs
+tar cvzf rfcpop-package.tar.gz --exclude-vcs --exclude "html-rfcs" package.sh README.txt urls.py settings.py rfcpop/ apache/ $libs
